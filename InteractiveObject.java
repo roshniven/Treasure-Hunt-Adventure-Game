9369286232a629ccbbdfa6947d3e0cb8665ec7fc
roshniven <126984825+roshniven@users.noreply.github.com>
2024-01-04 21:56:36 -0600

    Update InteractiveObject.java

--- InteractiveObject.java
+++ InteractiveObject.java
@@ -1,30 +1,3 @@
-//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
-//
-// Title: Interactive Object
-// Course: CS 300 Spring 2022
-//
-// Author: Roshni Venkat
-// Email: rvenkat@wisc.edu
-// Lecturer: Hobbes LeGault
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name: Shraddha Byndoor
-// Partner Email: sbyndoor@wisc.edu
-// Partner Lecturer's Name: Mouna Kacem
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-// __X_ Write-up states that pair programming is allowed for this assignment.
-// __X_ We have both read and understand the course Pair Programming Policy.
-// __X_ We have registered our team prior to the team registration deadline.
-//
-///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
-//
-// Persons: NONE
-// Online Sources: NONE
-//
-///////////////////////////////////////////////////////////////////////////////
-
 import processing.core.PApplet;
 import processing.core.PImage;
 import java.util.ArrayList;
